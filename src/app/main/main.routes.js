6c4862e34f96d7d9892c940c0b10ba71623fc21e
Rodrigo Troncoso <rod.tronco@gmail.com>
2015-10-21 03:38:08 -0300

    Fix routing bug affecting heroku deployed target

--- src/app/main/main.routes.js
+++ src/app/main/main.routes.js
@@ -9,13 +9,13 @@
   function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
-        url: '/home',
+        url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
       });
 
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/');
   }
 
 })();
